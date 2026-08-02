ba7e469ee3fb3b3c4c231c48b38485c24ce5736e
Uttkarsh Singh <72338896+u1krsh@users.noreply.github.com>
2026-08-02 11:41:55 +0530

    rev_mat

---
+++ Arrays/rev_matrix.java
@@ -0,0 +1,51 @@
+package Arrays;
+
+
+import java.util.*;
+
+
+public class rev_matrix {
+    static void reverse(int[][] arr){
+        int n = arr.length;
+        for(int i = 0;i<n;i++){
+            int left =0;
+            int right = n-1;
+            while(left < right){
+                int temp = arr[i][left];
+                arr[i][left] = arr[i][right];
+                arr[i][right] = temp;
+                left ++;
+                right--;
+            }
+        }
+    }
+
+
+    static void rotate(int[][] arr){
+        int n = arr.length;
+
+        for(int i = 0; i <n-1;i++){
+            for(int j = i+1;j<n;j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        reverse(arr);
+    }
+    public static void main(String[] args) {
+
+        int[][] arr = {{1,1,1,1},
+                {1,0,1,1},
+                {1,1,0,1},
+                {0,1,1,1}};
+        rotate(arr);
+        for(int k =0; k<arr.length;k++) {
+            for (int l = 0; l < arr[0].length; l++) {
+                System.out.print(arr[k][l] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
--- practice.java
+++ practice.java
@@ -3,50 +3,41 @@ import java.util.*;
 
 
 public class practice {
-    static void set_zer_met(int[][] arr) {
-        int n = arr.length;
-        int m = arr[0].length;
-        int col0 = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (arr[i][j] == 0) {
-                    arr[i][0] = 0;
-                    if (j != 0) {
-                        arr[0][j] = 0;
-                    } else {
-                        col0 = 0;
-                    }
+        static void reverse(int[][] arr){
+            int n = arr.length;
+            for(int i = 0;i<n;i++){
+                int left =0;
+                int right = n-1;
+                while(left < right){
+                    int temp = arr[i][left];
+                    arr[i][left] = arr[i][right];
+                    arr[i][right] = temp;
+                    left ++;
+                    right--;
                 }
             }
         }
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (arr[i][j] != 0) {
-                    if (arr[i][0] == 0 || arr[0][j] == 0) {
-                        arr[i][j] = 0;
-                    }
-                }
-            }
-        }
-        if (arr[0][0] == 0) {
-            for (int j = 0; j < m; j++) {
-                arr[0][j] = 0;
-            }
-        }
-        if (col0 == 0) {
-            for(int i =0;i<n;i++){
-                arr[i][0] = 0;
-        }
-    }
 
-    }
+
+        static void rotate(int[][] arr){
+            int n = arr.length;
+
+            for(int i = 0; i <n-1;i++){
+                for(int j = i+1;j<n;j++){
+                    int temp = arr[i][j];
+                    arr[i][j] = arr[j][i];
+                    arr[j][i] = temp;
+                }
+            }
+            reverse(arr);
+        }
     public static void main(String[] args) {
 
         int[][] arr = {{1,1,1,1},
                 {1,0,1,1},
                 {1,1,0,1},
                 {0,1,1,1}};
-        set_zer_met(arr);
+        rotate(arr);
         for(int k =0; k<arr.length;k++) {
             for (int l = 0; l < arr[0].length; l++) {
                 System.out.print(arr[k][l] + " ");
