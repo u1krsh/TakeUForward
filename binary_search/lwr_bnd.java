6b74626a89c3274e29d0fb76b7449b19be0e2968
Uttkarsh Singh <72338896+u1krsh@users.noreply.github.com>
2026-08-16 12:27:13 +0530

    lower_bound

--- binary_search/lwr_bnd.java
+++ binary_search/lwr_bnd.java
@@ -1,7 +1,25 @@
 package binary_search;
 
 public class lwr_bnd {
-    public static void main(String[] args) {
+    static int lower_bound(int[] arr, int t){
+        int n = arr.length;
+        int low = 0,  high = n-1;
+        int ans = n;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid] >= t){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid +1;
+            }
+        }
+        return ans;
+    }
 
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,3,7,8,9,9,9,11};
+        System.out.println(lower_bound(arr,6));
     }
 }
