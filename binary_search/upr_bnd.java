d8f2ec9844e7072cfa85bbb035bb482706158932
Uttkarsh Singh <72338896+u1krsh@users.noreply.github.com>
2026-08-16 12:28:51 +0530

    upper_bound

---
+++ binary_search/upr_bnd.java
@@ -0,0 +1,25 @@
+package binary_search;
+
+public class upr_bnd {
+    static int upper_bound(int[] arr, int t){
+        int n = arr.length;
+        int low = 0,  high = n-1;
+        int ans = n;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid] > t){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid +1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,3,7,8,9,9,9,11};
+        System.out.println(upper_bound(arr,2));
+    }
+}
