6a76dbecfe9821fdd13357e611d90584197533cd
Uttkarsh Singh <72338896+u1krsh@users.noreply.github.com>
2026-08-19 19:41:14 +0530

    practice

--- practice.java
+++ practice.java
@@ -3,29 +3,29 @@ import java.util.*;
 
 
 public class practice {
-    static int[][] merge_overlap(int[][] arr){
-        int n = arr.length;
-        Arrays.sort(arr,(a,b) ->a[0]-b[0]);
-        List<int[]> ans = new ArrayList<>();
-        for(int i = 0;i<n;i++){
-            if(ans.isEmpty() || arr[i][0]>ans.get(ans.size()-1)[0]){
-                ans.add(arr[i]);
+    static int maxSubSum(int[] arr){
+        int sum =0;
+        int maxi = Integer.MIN_VALUE;
+
+        for(int i =0;i<arr.length;i++){
+            sum += arr[i];
+            if(sum>maxi){
+                maxi = sum;
             }
-            else{
-                ans.get(ans.size()-1)[0] = Math.max(ans.get(ans.size()-1)[1], arr[i][1]);
+            if(sum<0){
+                sum = 0;
             }
         }
-        return ans.toArray(new int[ans.size()][]);
+        return sum;
     }
-    public static void main(String[] args) {
 
-        int[] arr = {-2,-2,-2,-2,-1,-1,-1,0,0,0,2,2,2,2};
-//        System.out.println(three_sum(arr));
-////        for(int k =0; k<arr.length;k++) {
-////            for (int l = 0; l < arr[0].length; l++) {
-////                System.out.print(arr[k][l] + " ");
-////            }
-//            System.out.println();
+
+
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,2,1,0,0,0,2,2,1};
+        System.out.println(maxSubSum(arr));
+//        for(int i =0;i<arr.length;i++){
+//            System.out.print(arr[i]+" ");
 //        }
     }
 
