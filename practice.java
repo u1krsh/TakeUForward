d3f205c4a13915ccaad48a7fc5d42ce0ad86ab0c
Uttkarsh Singh <72338896+u1krsh@users.noreply.github.com>
2026-08-20 14:54:58 +0530

    gcd

--- practice.java
+++ practice.java
@@ -3,30 +3,49 @@ import java.util.*;
 
 
 public class practice {
-    static int maxSubSum(int[] arr){
-        int sum =0;
-        int maxi = Integer.MIN_VALUE;
+    static int gdc(int a, int b){
+        int res = 1;
+        int pa=2,pb =2;
+        while(a>1&&b>1){
+            while(pa*pa<=a && a%pa !=0) pa++;
+                int fac;
+                if(pa*pa>a){
+                    fac =a;
+                }
+                else{
+                    fac = pa;
+                }
 
-        for(int i =0;i<arr.length;i++){
-            sum += arr[i];
-            if(sum>maxi){
-                maxi = sum;
+
+
+            while(pb*pb<=b && b%pb != 0 ) pb++;
+            int fb;
+            if(pb*pb >b){
+                fb = b;
             }
-            if(sum<0){
-                sum = 0;
+            else{
+                fb = pb;
+            }
+
+            if (fac==fb){
+                res *= fac;
+                a = a/fac;
+                b= b/fb;
+            }
+            else if(fac<fb){
+                a =a /fac;
+            }
+            else{
+                b = b/fb;
             }
         }
-        return sum;
+        return res;
     }
 
 
 
     public static void main(String[] args) {
-        int[] arr = {0,1,2,2,1,0,0,0,2,2,1};
-        System.out.println(maxSubSum(arr));
-//        for(int i =0;i<arr.length;i++){
-//            System.out.print(arr[i]+" ");
-//        }
+        System.out.println(gdc(6,12));;
     }
 
 }
